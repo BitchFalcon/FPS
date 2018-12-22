1bf38312ef8f4958c1d00c575ad865b099407df5
BitchFalcon <46070796+BitchFalcon@users.noreply.github.com>
2018-12-22 21:59:33 +0000

    changed shooting script

---
+++ FPS/Assets/Scripts/Weapons/Shooting.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+
+    int whichBarrel = 1;
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public ParticleSystem muzzleFlash1;
+    public GameObject impactEffect;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+
+            Shoot();
+        }
+        if (Input.GetButtonDown("Fire2"))
+        {
+
+            Shoot2();
+        }
+
+        
+	}
+
+    void Shoot()
+    {
+        if(whichBarrel == 1)
+        {
+            muzzleFlash1.Play();
+            
+            whichBarrel++;
+        }
+        else
+        {
+            muzzleFlash.Play();
+            whichBarrel--;
+        }
+        
+        
+        RaycastHit hit;
+       if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+        Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+
+            }
+            Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        }
+
+
+    }
+    void Shoot2()
+    {
+        
+            muzzleFlash1.Play();
+            muzzleFlash.Play();  
+
+
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+
+            }
+            Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        }
+
+
+    }
+}
